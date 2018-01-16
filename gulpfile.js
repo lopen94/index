7b615d205c94446781fdc20df24f01b1cd1c4823
k.bieganowski <k.bieganowski@x-coding.pl>
2018-01-16 08:51:18 +0100

    sass structure

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ const prefix = require('gulp-autoprefixer');
 const imagemin = require('gulp-imagemin');
 
 gulp.task('sass', function () {
-  	gulp.src('./src/scss/*.scss')
+  	gulp.src('./src/scss/**/*.scss')
     .pipe(sass.sync().on('error', sass.logError))
     .pipe(prefix('last 2 versions'))
     .pipe(gulp.dest('dist/css'));
@@ -16,6 +16,6 @@ gulp.task('copyHTML', function(){
 });
 
 gulp.task('watch', function(){
-	gulp.watch('./src/scss/*.scss', ['sass']);
+	gulp.watch('./src/scss/**/*.scss', ['sass']);
 	gulp.watch('./src/template/*.html', ['copyHTML']);
 });
